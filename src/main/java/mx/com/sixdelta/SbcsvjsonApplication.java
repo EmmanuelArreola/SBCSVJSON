7aae2ed1185879999db26ebc9fd43082c72b6940
EmmanuelArreola <emmanuel.arreola@cloudgensys.com>
2022-04-27 15:30:44 -0500

    First commit with all the project

---
+++ src/main/java/mx/com/sixdelta/SbcsvjsonApplication.java
@@ -0,0 +1,13 @@
+package mx.com.sixdelta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbcsvjsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbcsvjsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/sixdelta/SbcsvjsonApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.sixdelta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbcsvjsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
